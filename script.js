fb0ecb455bf4ea242a5470eef718a044ebd8d744
Pavan Pandey <ppawan019@gmail.com>
2023-09-24 11:42:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const btn=document.getElementById("enterBtn");
+const para =document.getElementById("status");
+btn.addEventClickListener("click" ,()=>{
+	 para.innerText="Entered Metaverse";
+});
+
